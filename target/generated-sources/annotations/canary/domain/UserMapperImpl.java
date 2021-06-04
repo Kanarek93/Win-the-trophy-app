5598a72f7f7b7a92976df832d53ed19efab4ee98
Kanarek93 <darenka93@gmail.com>
2021-06-04 15:14:25 +0200

    Mapper works

---
+++ target/generated-sources/annotations/canary/domain/UserMapperImpl.java
@@ -0,0 +1,41 @@
+package canary.domain;
+
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-04T14:49:49+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
+)
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User userDtoToUser(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userDto.getName() );
+        user.setPassword( userDto.getPassword() );
+        user.setEmail( userDto.getEmail() );
+
+        return user;
+    }
+
+    @Override
+    public UserDto userToUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setName( user.getName() );
+        userDto.setEmail( user.getEmail() );
+        userDto.setPassword( user.getPassword() );
+
+        return userDto;
+    }
+}
